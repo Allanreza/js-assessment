8e5bb557ec9d5d0d5e5d8916600cd240ad0df86f
Allan <allanrzturk@gmail.com>
2017-06-08 19:25:59 -0700

    finished assessment

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //#1
 //Declare a variable called 'name' that has the value of your name.
-
+var name = 'Allan';
 /*code here*/
 
 //#2
@@ -8,18 +8,24 @@
 //If yes, log 'yes'
 //otherwise log 'no'
 //note: use console.log to log the string
-
+if (name === "Ben"){
+  console.log('yes');
+} else {
+  console.log('no');
+}
 /*code here*/
 
 //#3
 //create an array called 'favoriteFoods'.
 //fill it up with the names of several of your favorite foods
-
+var favoriteFoods = ['Pasta', 'Chicken', 'Soup', 'Fish', 'Okra'];
 /*code here*/
 
 //#4
 //use a for loop to log each food in the 'favoriteFoods' array
-
+for (var i = 0; i < favoriteFoods.length; i++){
+  console.log(favoriteFoods[i]);
+}
 /*code here*/
 
 //#5
@@ -27,14 +33,20 @@
 //give 'favoriteMovie' a property called 'runtime' and set it equal to how long the movie is in minutes
 //give 'favoriteMovie' a property called 'title' and set it equal to the title
 //give 'favoriteMovie' a property called 'director' and set it equal to the director's name
+var favoriteMovie = {};
 
+favoriteMovie['runtime'] = '152m';
+favoriteMovie['title'] = 'The Dark Knight';
+favoriteMovie['director'] = 'Christopher Nolan';
 /*code here*/
 
 //#6
 //create a function called 'sayHi'.
 //'sayHi' should accept one argument called 'name'
 //when 'sayHi' is invoked it should log the string 'Hello <name>!' where <name> is equal to the 'name' argument
-
+function sayHi(name){
+  console.log('Hello ' + name + "!");
+}
 /*code here*/
 
 //#7
@@ -42,20 +54,32 @@
 //create three objects that have information about your friends
 //each object should have a 'name', 'age', and 'vocation' property with appropriate data
 //put the three objects inside of the 'friends' array
-
+var friends = [
+  {name: 'Matt', age: 23, vocation: 'Plumbing'},
+  {name: 'Sam', age: 24, vocation: 'Writing'},
+  {name: 'Christian', age: 26, vocation: 'Producing'}
+];
 /*code here*/
 
 //#8
 //use a for loop to iterate over the 'friends' array from problem #7
 //inside the for loop print the string 'My friend <name> is <age> years old and does <vocation> for work.'
-
+for (var i in friends){
+  console.log('My friend ' + friends[i].name + ' is ' + friends[i].age + ' years old and does ' + friends[i].vocation + ' for work.');
+}
 /*code here*/
 
 //#9
 //create a constructor called 'User' that can function as a class for creating new user objects
-//'User' should take 'email', and 'password' as arguments 
+//'User' should take 'email', and 'password' as arguments
 //each instance of 'User' that is created should have a 'email', and 'password' property that is equal to the arguments passed to the constructor
+function User(email, password){
+  this.email = email;
+  this.password = password;
+}
 
+var firstUser = new User('allanrzturk@gmail.com', 'HiFives555');
+console.log(firstUser);
 /*code here*/
 
 //#10
@@ -63,5 +87,13 @@
 //'nFactorial' should return the factorial of the 'num' argument
 //note: the factorial of 5 is (5 * 4 * 3 * 2 * 1) ->  120
 //try to solve this recursively
-
+function nFactorial(num){
+  if (num < 0){
+    return -1;
+  } else if (num === 0){
+    return 1;
+  } else {
+    return (num * nFactorial(num - 1));
+  }
+}
 /*code here*/
